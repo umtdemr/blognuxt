af1956e49ff953ee165ad97ebb513a5f1f66143c
umit <umit@bypassdigital.com>
2020-03-02 03:42:16 +0300

    vue select added

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,13 @@
       <div class="page-content">
           <h2 class="page-content__title">Blog</h2>
           <div class="main_hr"></div>
-          <div class="post__filter">
-              <select name="" id="">
-                  <option value="recent">En yeniye göre</option>
-                  <option value="recent">En çok okunana göre</option>
-                  <option value="recent">En eskiye göre</option>
-              </select>
+          <div class="post__filters">
+              <div class="post__category">
+                  <v-select multiple v-model="selectedCategory" :options="['Genel (230)','Film', 'Yazılım']"></v-select>
+                </div>
+            <div class="post__filter">
+                <v-select v-model="selectedFilter" :options="['En yeni','En çok okunan', 'En eski']"></v-select>
+            </div>
           </div>
           <div class="posts">
               <SinglePost v-for="post in posts" :post="post" :key="post.slug"/>
@@ -16,11 +17,14 @@
 </template>
 
 <script>
+import 'vue-select/dist/vue-select.css';
 import SinglePost from '@/components/posts/SinglePost'
+import vSelect from 'vue-select'
 
 export default {
     components: {
-        SinglePost
+        SinglePost,
+        vSelect
     },
     data() {
         return {
@@ -33,7 +37,9 @@ export default {
                     previewText: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident totam nam ex dolores. Tenetur amet autem nostrum, molestias adipisci soluta ipsum consectetur. Necessitatibus porro odio quidem quaerat error nostrum! Quidem! Veritatis a placeat incidunt ipsum laudantium eveniet et architecto fuga, hic repellendus fugit temporibus, voluptas at cupiditate dolor sapiente in, id maiores sunt molestiae doloremque dolorem nostrum minus debitis! Amet. Quis repudiandae cumque illum aut veritatis optio accusamus expedita officia ipsum nam ducimus magni vel fugiat ratione, quas dicta deserunt ad reiciendis obcaecati tempora consectetur magnam natus exercitationem iusto? Maiores.'
                     
                 },
-            ]
+            ],
+            selectedCategory: ['Genel (230)'],
+            selectedFilter: 'En yeni'
         }
     }
 }
